8811114f22f48235ab19a6898f4363d78ea144a5
Minori Hiraoka <minori@minori.moe>
2015-08-19 23:14:18 +0900

    Change actionbar color to red

--- app/src/main/java/moe/minori/currencyinfo/SettingActivity.java
+++ app/src/main/java/moe/minori/currencyinfo/SettingActivity.java
@@ -3,33 +3,47 @@ package moe.minori.currencyinfo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingActivity extends PreferenceActivity {
+public class SettingActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.pref_general);
-
-        findPreference("enabledCheckBox").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-
-                if ((boolean) newValue) {
-                    // plugin enabled
-
-                    startService(new Intent(SettingActivity.this, CurrencyLookupService.class));
-
-                } else {
-                    // plugin disabled
-
-                    stopService(new Intent(SettingActivity.this, CurrencyLookupService.class));
-                }
-                return true;
-            }
-        });
-
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingFragment()).commit();
     }
+
+
+    public static class SettingFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.pref_general);
+
+            findPreference("enabledCheckBox").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+                    if ((boolean) newValue) {
+                        // plugin enabled
+
+                        getActivity().startService(new Intent(getActivity(), CurrencyLookupService.class));
+
+                    } else {
+                        // plugin disabled
+
+                        getActivity().stopService(new Intent(getActivity(), CurrencyLookupService.class));
+                    }
+                    return true;
+                }
+            });
+
+        }
+    }
+
 }
+
+
+
